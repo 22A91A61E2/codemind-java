ef3719787028e48b624eef1c6fd5fcd1efe2400f
B.Sravya <119840452+22A91A61E2@users.noreply.github.com>
2023-08-05 11:24:41 +0530

    import java.util.Scanner; public class multi{     public static void main(String args[]){         Scanner sc= new Scanner(System.in);         int number1= sc.nextInt();         int number2= sc.nextInt();         int number3=number1+number2;         System.out.printf("%d",number3);
        }
    }

---
+++ Adding_Two_Numbers.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class multi{
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        int number1= sc.nextInt();
+        int number2= sc.nextInt();
+        int number3=number1+number2;
+        System.out.printf("%d",number3);
+        
+    }
+}
