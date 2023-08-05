9987b1870f3a89ae2e124c57d3a9b3fb0e5c6d11
B.Sravya <119840452+22A91A61E2@users.noreply.github.com>
2023-08-05 11:56:15 +0530

    import java.util.Scanner; public class multi{     public static void main(String args[])     {         Scanner sc= new Scanner(System.in);         double r= sc.nextInt();         double a = 3.14*r*r;         System.out.printf("%.2f",a);     } }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class multi{
+    public static void main(String args[])
+    {
+        Scanner sc= new Scanner(System.in);
+        double r= sc.nextInt();
+        double a = 3.14*r*r;
+        System.out.printf("%.2f",a);
+    }
+}
